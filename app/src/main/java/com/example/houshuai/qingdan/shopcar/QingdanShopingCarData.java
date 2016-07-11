cb3536cc801ac30c9522425c33d1015e1fc20f77
Dufei <308039683@qq.com>
2016-07-11 15:21:48 +0800

    更新你猜是什么，就不告诉你

---
+++ app/src/main/java/com/example/houshuai/qingdan/shopcar/QingdanShopingCarData.java
@@ -0,0 +1,18 @@
+package com.example.houshuai.qingdan.shopcar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Dufei on 2016/7/11.
+ */
+
+public class QingdanShopingCarData {
+    /**保存添加到购物车的数据*/
+    public static int arrayList_cart_id=0;
+    /**保存添加到购物车的数据*/
+    public static ArrayList<HashMap<String, Object>> arrayList_cart=new ArrayList<HashMap<String,Object>>();
+    /**保存购物车中选择的商品的总价数据*/
+    public static float Allprice_cart=0;
+
+}
